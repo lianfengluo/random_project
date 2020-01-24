3722b44e6755038f2c7c26f3c1f60a0419b8c06c
lianfengluo <lianfengluo28@gmail.com>
2020-01-24 12:36:27 +1100

    Updates

---
+++ precache-manifest.6b9d835b9f08f610c7460bdaab85b5c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795e7ee545b746ee5d6b20f444d8da8d",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "9979c364b0c7bf00c62a",
+    "url": "/project1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8e4eb2e7674d338d3446",
+    "url": "/project1/static/js/2.d3ab5617.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project1/static/js/2.d3ab5617.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9979c364b0c7bf00c62a",
+    "url": "/project1/static/js/main.48654a28.chunk.js"
+  },
+  {
+    "revision": "06b763bd142468b289f3",
+    "url": "/project1/static/js/runtime-main.f4cb27da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project1/static/media/logo.5d5d9eef.svg"
+  }
+]);
